736113d622cf600bd4b0421e32878bc72161a676
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-15 01:49:02 +0200

    header - mika

--- Views/header.php
+++ Views/header.php
@@ -1,6 +1,6 @@
 <?php
 ?>
-<div id="header_1" class="navbar">
+<div id="header_1">
 	<div id="header_logo">
 		<!--<img alt="logo" src="/assets/images/Hyperion-yellow-transparent.png" class="img-fluid"> -->
 	</div>
