c15287ea8c376692182843abb818c22c005e1760
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-15 02:58:57 +0200

    footer - mika

--- Views/footer.php
+++ Views/footer.php
@@ -1,3 +1,9 @@
 <div id="footer">
+    <div id="footer_line"></div>
+    <div id="footer_1">
 
+    </div>
+    <div id="footer_2">
+        <p>©  <?php echo date('Y'); ?>, Hyperion.fr, Groupe 1</p>
+    </div>
 </div>
