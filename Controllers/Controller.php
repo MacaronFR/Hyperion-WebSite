8d73c9ca142ea5bc2cce951e83ef7f86fec514f5
MacaronFR <denisft77@gmail.com>
2021-04-15 11:04:30 +0200

    mise à jour du css mise à jour des controller pour faciliter la mise en place de la page

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -10,6 +10,50 @@ namespace Hyperion\WebSite;
  */
 abstract class Controller {
 	/**
+	 * Return the default head with $title as <title>
+	 * @param string $title Title of the page
+	 * @return string Return the default head
+	 */
+	protected function prepareHead(string $title): string{
+		ob_start();
+		include "Views/head.php";
+		return ob_get_clean();
+	}
+
+	/**
+	 * Return the default header
+	 * @return string Default header
+	 */
+	protected function prepareHeader(): string{
+		ob_start();
+		include "Views/header.php";
+		return ob_get_clean();
+	}
+
+	/**
+	 * Return the default footer
+	 * @return string Default footer
+	 */
+	protected function prepareFooter(): string{
+		ob_start();
+		include "Views/footer.php";
+		return ob_get_clean();
+	}
+
+	/**
+	 * Return the default body of the page
+	 * @param string $header Header of the page
+	 * @param string $main Main content of the page
+	 * @param string $footer Footer of the page
+	 * @return string Body of the page
+	 */
+	protected function prepareBody(string $header, string $main, string $footer): string{
+		ob_start();
+		include "Views/body.php";
+		return ob_get_clean();
+	}
+
+	/**
 	 * Must be instanced for using the get() method and control GET request
 	 * @param array $args Argument passed to the controller by the router
 	 * @return mixed
--- Controllers/StoreController.php
+++ Controllers/StoreController.php
@@ -10,27 +10,13 @@ class StoreController extends Controller {
 	 * @inheritDoc
 	 */
 	public function get(array $args){
-		$title = "Hyperion";
-		ob_start();
-		include "Views/head.php";
-		$head = ob_get_contents();
-		ob_end_clean();
-		ob_start();
-		include "Views/header.php";
-		$header = ob_get_contents();
-		ob_end_clean();
+		$head = $this->prepareHead("Hyperion");
+		$header = $this->prepareHeader();
 		$main = "";
-        ob_start();
-        include "Views/footer.php";
-		$footer = ob_get_contents();
-        ob_end_clean();
-		ob_start();
-		include "Views/body.php";
-		$body = ob_get_contents();
-		ob_end_clean();
+        $footer = $this->prepareFooter();
+		$body = $this->prepareBody($header, $main, $footer);
 		include "Views/root.php";
 	}
-	
 	/**
 	 * @inheritDoc
 	 */
