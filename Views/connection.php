82f5c07828df426bc3f19c5b2d8c20a946a18c42
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-17 13:56:12 +0200

    connexion - mika

--- Views/connection.php
+++ Views/connection.php
@@ -1,7 +1,7 @@
 <?php
 ?>
 <div id="div_form_connexion" class="container">
-    <div id="div_connexion">
+    <div id="div_connexion" class="col-4">
         <h2>S'identifier</h2>
         <form action="#" method="post">
             <div class="mb-3">
@@ -17,7 +17,7 @@
         </div>
     </div>
 
-    <div id="div_connexion_2">
+    <div id="div_connexion_2" class="col-4">
         <div>
             <h2>Nouveau chez Hyperion ?</h2>
         </div>
