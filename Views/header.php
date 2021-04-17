cbbd15002fce50803a26d2bc452beaa0d68ab82a
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-17 17:58:44 +0200

    connexion - mika

--- Views/header.php
+++ Views/header.php
@@ -4,7 +4,7 @@
 <!-- Header sans barre de recherche -->
 <nav id="header_1" class="navbar py-5 py-lg-2">
     <a class="navbar-brand" href="#">
-        <img src="/assets/images/Hyperion-yellow-transparent.png" alt="logo_Hyperion" width="30" height="30">
+        <img src="/assets/images/Hyperion-yellow-transparent.png" alt="logo_Hyperion" class="img-fluid">
     </a>
 </nav>
 <div id="header_1_line" class="navbar py-2 py-lg-0.1"></div>
