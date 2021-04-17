231cb00ac3570616f253ca38164aa233a1e04ff7
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-17 12:48:26 +0200

    connexion - mika

--- Views/connection.php
+++ Views/connection.php
@@ -1,3 +1,23 @@
 <?php
 ?>
-<p>Hi</p>
+
+<div id="div_connexion">
+    <h2>S'identifier</h2>
+    <form action="#" method="post">
+        <div class="">
+            <label for="id" class="form-label">Adresse e-mail ou numéro de téléphone portable</label>
+            <input type="text" class="form-control" name="id">
+        </div>
+        <div class="">
+            <input type="submit" name="form_connexion" value="Continuer">
+        </div>
+    </form>
+    <div class="">
+        <p>En passant votre commande, vous acceptez les Conditions générales de vente d'Hyperion.</p>
+    </div>
+
+    <div id="div_connexion_2">
+
+    </div>
+
+</div>
