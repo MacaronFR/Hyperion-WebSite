b1c88dbc56daba8999033a917766b7d394727e80
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-18 01:51:29 +0200

    header_2 - mika

--- Views/header_2.php
+++ Views/header_2.php
@@ -3,6 +3,9 @@
 
 <!-- Header sans barre de recherche -->
 <nav id="header_2" class="navbar navbar-expand-lg py-4 py-lg-0">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
     <a class="navbar-brand" href="/shop">
         <img src="/assets/images/Hyperion-yellow-transparent.png" alt="logo_Hyperion" class="img-fluid">
     </a>
