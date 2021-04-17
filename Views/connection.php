3f0e3c616519ae02a1bf3e5612e3d8943a57d378
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-17 16:20:08 +0200

    connexion - mika

--- Views/connection.php
+++ Views/connection.php
@@ -1,28 +1,28 @@
 <?php
 ?>
 <div id="div_form_connexion" class="container">
-    <div id="div_connexion" class="row">
+    <div id="div_connexion" class="">
         <h2>S'identifier</h2>
-       <!-- <form action="#" method="post">
-            <div class="mb-3">
+        <form action="#" method="post">
+            <div class="form-group">
                 <label for="id" class="form-label">Adresse e-mail ou numéro de téléphone portable</label>
                 <input type="text" class="form-control" name="id">
             </div>
-            <div class="">
+            <div class="form-check">
                 <input type="submit" name="form_connexion" value="Continuer">
             </div>
         </form>
         <div class="">
             <p>En passant votre commande, vous acceptez les Conditions générales de vente d'Hyperion.</p>
-        </div> -->
+        </div>
     </div>
 
     <div id="div_connexion_2" class="">
         <div>
             <h2>Nouveau chez Hyperion ?</h2>
         </div>
-     <!--   <div class="">
+        <div class="form-check">
             <input type="submit" name="form_connexion" value="Continuer">
         </div>
-    </div> -->
+    </div>
 </div>
