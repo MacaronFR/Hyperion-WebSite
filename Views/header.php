88fef0042ca00e4c512892566d91c526047a70e2
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-17 17:17:35 +0200

    connexion - mika

--- Views/header.php
+++ Views/header.php
@@ -2,11 +2,9 @@
 ?>
 
 <!-- Header sans barre de recherche -->
-<nav id="header_1" class="navbar">
-	<div id="header_logo">
-        <a class="navbar-brand" href="#">
-		    <img src="/assets/images/Hyperion-yellow-transparent.png" alt="logo_Hyperion">
-        </a>
-    </div>
+<nav id="header_1" class="navbar navbar-dark">
+    <a class="navbar-brand" href="#">
+        <img src="/assets/images/Hyperion-yellow-transparent.png" alt="logo_Hyperion">
+    </a>
     <div id="header_1_line"></div>
 </nav>
