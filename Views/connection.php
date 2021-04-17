0eb67e057cdbea9cc3ce7d0a918378c2755912f8
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-17 19:14:28 +0200

    connexion - mika

--- Views/connection.php
+++ Views/connection.php
@@ -1,7 +1,7 @@
 <?php
 ?>
 <div id="div_form_connexion" class="container-fluid d-flex justify-content-center">
-    <div id="div_connexion" class="row col-8">
+    <div id="div_connexion" class="row col-10">
         <h2>S'identifier</h2>
         <form action="#" method="post">
             <div class="form-group">
