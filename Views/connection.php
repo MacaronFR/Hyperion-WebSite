6db8139c7967257693bb97b27b932876157d303c
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-17 22:51:45 +0200

    connexion - mika

--- Views/connection.php
+++ Views/connection.php
@@ -26,7 +26,7 @@
             <h5>Nouveau chez Hyperion ?</h5>
         </div>
         <div class="form-group mt-4 mt-lg-4">
-            <input class="btn btn-primary" type="button" value="Créer votre compte Hyperion">
+            <button class="btn btn-primary" type="submit" value="Créer votre compte Hyperion"></button>
         </div>
     </div>
 </div>
