fe9e70f83b0ef951a9f71701334408fffa465199
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-19 01:28:31 +0200

    header - 2

--- Views/header_2.php
+++ Views/header_2.php
@@ -2,27 +2,11 @@
 ?>
 
 <nav id="header_2" class="navbar navbar-expand-lg py-4 py-lg-0">
-    <a class="" href="/shop">
-        <img src="/assets/images/Hyperion-yellow-transparent.png" alt="logo_Hyperion" class="img-fluid">
-    </a>
-     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader2" aria-controls="navbarHeader2" aria-expanded="false">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <!-- deuxieme partie -->
-    <div class="navbar" id="navbarHeader2">
-        <div class="">
-            <div class="">
-                <?php if (isset($_SESSION['mail'])){ ?>
-                    <a class="nav-item nav-link" href="#">Mon compte</a>
-                    <a class="nav-item nav-link" href="#">Mes commandes</a>
-                    <a class="nav-item nav-link" href="/disconnect">Déconnexion</a>
-                    <?php }else { ?>
-                    <a class="nav-item nav-link" href="/inscription">Inscription</a>
-                    <a class="nav-item nav-link" href="/connect">Connexion</a>
-                <?php } ?>
-            </div>
-        </div>
+    <div class="container-fluid">
+        <a class="navbar-brand">Navbar</a>
+        <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
     </div>
 </nav>
-
