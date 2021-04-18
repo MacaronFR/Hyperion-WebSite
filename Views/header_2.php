47dcd0b3fbb69857d22913c36398b1d4552ae3ef
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-19 01:38:37 +0200

    header - 2

--- Views/header_2.php
+++ Views/header_2.php
@@ -4,9 +4,14 @@
 <nav id="header_2" class="navbar navbar-expand-lg py-4 py-lg-0">
     <div class="container-fluid">
         <a class="navbar-brand">Navbar</a>
-        <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        <div class="input-group mb-3">
+            <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">aaaaaaaa</a></li>
+                <li><a class="dropdown-item" href="#">bbbbbbbb</a></li>
+                <li><a class="dropdown-item" href="#">cccccccc</a></li>
+            </ul>
+            <input type="text" class="form-control" aria-label="Text input with dropdown button">
+        </div>
     </div>
 </nav>
