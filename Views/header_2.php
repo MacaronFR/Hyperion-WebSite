96181e1c643231fc541d8aab5e0d49cc7d66a36a
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-18 03:11:46 +0200

    header_2 - mika

--- Views/header_2.php
+++ Views/header_2.php
@@ -1,15 +1,6 @@
 <?php
 ?>
 
-<!-- Header sans barre de recherche -->
-<!--
-<nav id="header_2" class="navbar navbar-expand-lg py-4 py-lg-0">
-    <a class="navbar-brand" href="/shop">
-        <img src="/assets/images/Hyperion-yellow-transparent.png" alt="logo_Hyperion" class="img-fluid">
-    </a>
-</nav>
--->
-
 <nav id="header_2" class="navbar navbar-expand-lg py-4 py-lg-0">
     <a class="navbar-brand" href="/shop">
         <img src="/assets/images/Hyperion-yellow-transparent.png" alt="logo_Hyperion" class="img-fluid">
