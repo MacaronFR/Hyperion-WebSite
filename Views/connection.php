8cb0e5a4ddb5b9c997292a7a3153f27f61d07824
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-18 02:54:54 +0200

    header_2 - mika

--- Views/connection.php
+++ Views/connection.php
@@ -1,7 +1,7 @@
 <?php
 ?>
-<div id="div_form_connexion" class="container-fluid d-flex flex-column mt-16 mt-lg-5">
-    <div id="div_connexion" class="row col-6 col-lg-4 border border-2 rounded-3 py-4 px-4 align-self-center">
+<div id="div_form_connexion" class="container-fluid d-flex flex-column mt-11 mt-lg-5">
+    <div id="div_connexion" class="row col-11 col-lg-4 border border-2 rounded-3 py-4 px-4 align-self-center">
         <h1>S'identifier</h1>
         <form action="#" method="post">
             <div class="form-group mt-1 mt-lg-4">
@@ -21,7 +21,7 @@
         </div>
     </div>
 
-    <div id="div_connexion_2" class="row col-6 col-lg-4 py-4 px-4 align-self-center">
+    <div id="div_connexion_2" class="row col-11 col-lg-4 py-4 px-4 align-self-center">
         <div class="mt-3 mt-lg-4">
             <h5>Nouveau chez Hyperion ?</h5>
         </div>
--- Views/header.php
+++ Views/header.php
@@ -2,7 +2,7 @@
 ?>
 
 <!-- Header sans barre de recherche -->
-<nav id="header_1" class="navbar py-4 py-lg-0">
+<nav id="header_1" class="navbar py-1 py-lg-0">
     <a class="navbar-brand" href="/shop">
         <img src="/assets/images/Hyperion-yellow-transparent.png" alt="logo_Hyperion" class="img-fluid">
     </a>
