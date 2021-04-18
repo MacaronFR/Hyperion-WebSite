23a548e0a5d89a1772daee158bade773dc7a3c6f
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-19 01:32:36 +0200

    header - 2

--- Views/header_2.php
+++ Views/header_2.php
@@ -4,13 +4,9 @@
 <nav id="header_2" class="navbar navbar-expand-lg py-4 py-lg-0">
     <div class="container-fluid">
         <a class="navbar-brand">Navbar</a>
-        <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search">
-            <div class="input-group-btn">
-                <button class="btn btn-default" type="submit">
-                    <i class="glyphicon glyphicon-search"></i>
-                </button>
-            </div>
-        </div>
+        <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
     </div>
 </nav>
