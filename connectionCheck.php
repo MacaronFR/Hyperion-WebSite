1dcfb202115ab1e7a3c47b246cfbbdc04def9ac3
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-18 19:08:51 +0200

    header - 2

--- connectionCheck.php
+++ connectionCheck.php
@@ -3,6 +3,10 @@ session_start();
 $connected = isset($_SESSION['email']) ? true : false;
 $admin = isset($_SESSION['haunter']) ? true : false;
 $vendor = isset($_SESSION['vendor']) ? true : false;
-echo $_SESSION['mail'];
-echo "hi";
+ echo $_SESSION['id'];
+//$_SESSION['mail'];
+//$_SESSION['level'];
+//$_SESSION['name'];
+//$_SESSION['token'];
+
 ?>
