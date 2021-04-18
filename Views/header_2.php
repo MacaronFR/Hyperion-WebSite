559256fda087df399b7f1b9d9159020b4467678b
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-19 01:50:38 +0200

    header - 2

--- Views/header_2.php
+++ Views/header_2.php
@@ -6,14 +6,20 @@
         <a class="navbar-brand" href="/shop">
             <img src="/assets/images/Hyperion-yellow-transparent.png" alt="logo_Hyperion" height="70">
         </a>
-        <div class="input-group">
-            <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Toutes nos catégories</button>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">aaaaaaaa</a></li>
-                <li><a class="dropdown-item" href="#">bbbbbbbb</a></li>
-                <li><a class="dropdown-item" href="#">cccccccc</a></li>
-            </ul>
-            <input type="text" class="form-control" aria-label="Text input with dropdown button">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader2" aria-controls="navbarHeader2" aria-expanded="false">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div id="navbarHeader2">
+            <div class="input-group">
+                <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Toutes nos catégories</button>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">aaaaaaaa</a></li>
+                    <li><a class="dropdown-item" href="#">bbbbbbbb</a></li>
+                    <li><a class="dropdown-item" href="#">cccccccc</a></li>
+                </ul>
+                <input type="text" class="form-control" aria-label="Text input with dropdown button">
+            </div>
         </div>
     </div>
 </nav>
