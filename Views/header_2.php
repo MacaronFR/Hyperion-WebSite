fe28bd8c3bb21c422b48c2d7e4a9c89b4825b3a6
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-19 01:22:20 +0200

    header - 2

--- Views/header_2.php
+++ Views/header_2.php
@@ -2,7 +2,7 @@
 ?>
 
 <nav id="header_2" class="navbar navbar-expand-lg py-4 py-lg-0">
-    <a class="navbar-brand" href="/shop">
+    <a class="" href="/shop">
         <img src="/assets/images/Hyperion-yellow-transparent.png" alt="logo_Hyperion" class="img-fluid">
     </a>
      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader2" aria-controls="navbarHeader2" aria-expanded="false">
@@ -10,9 +10,9 @@
     </button>
 
     <!-- deuxieme partie -->
-    <div class="collapse navbar-collapse" id="navbarHeader2">
-        <div class="navbar-collapse">
-            <div class="d-flex flex-column flex-lg-row">
+    <div class="navbar" id="navbarHeader2">
+        <div class="">
+            <div class="">
                 <?php if (isset($_SESSION['mail'])){ ?>
                     <a class="nav-item nav-link" href="#">Mon compte</a>
                     <a class="nav-item nav-link" href="#">Mes commandes</a>
