e640f39619c0cadcc4e172f17852a59e813f4aac
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-19 03:09:56 +0200

    header - 2

--- Views/header_2.php
+++ Views/header_2.php
@@ -22,7 +22,7 @@
                     <input type="text" class="form-control" aria-label="Text input with dropdown button">
                 </div>
                 <div class="d-grid gap-2 mt-3 mt-lg-0">
-                    <button id="search_barre_bouton" class="btn btn-outline-success" type="submit">Search</button>
+                    <button id="search_barre_bouton" class="btn btn-outline-success rounded-lg-end" type="submit">Search</button>
                 </div>
             </div>
             <div class="navbar-nav me-lg-5" id="header_1_link">
