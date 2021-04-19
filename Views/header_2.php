9eb173f13eea0e478c767baad64f03a4f1323a78
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-19 02:10:54 +0200

    header - 2

--- Views/header_2.php
+++ Views/header_2.php
@@ -11,7 +11,7 @@
         </button>
 
         <div id="navbarHeader2" class="collapse navbar-collapse">
-            <div class="d-flex flex-column flex-lg-row container-fluid">
+            <div class="d-flex flex-column flex-lg-row container-md">
                 <div class="input-group">
                     <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Toutes nos catégories</button>
                     <ul class="dropdown-menu">
