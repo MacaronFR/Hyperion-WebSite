1bb28d10172ae2e6163d7be11b49ac7e91741056
MacaronFR <denisft77@gmail.com>
2021-04-20 16:53:22 +0200

    fix some behaviour on footer.php

--- Controllers/ShopController.php
+++ Controllers/ShopController.php
@@ -4,37 +4,37 @@
 namespace Hyperion\WebSite;
 require_once "autoload.php";
 
-class ShopController extends Controller
-{
+class ShopController extends Controller{
 
 	private function prepareShop(): string{
 		ob_start();
 		include "Views/shop.php";
 		return ob_get_clean();
 	}
-    /**
-     * @inheritDoc
-     */
-    public function get(array $args){
-    	$root = get_text("root");
-        $head = $this->prepareHead("Shop");
-        $header = $this->prepareHeader_2($root['header']);
-        $main = $this->prepareShop();
-        $footer = $this->prepareFooter();
-        $body = $this->prepareBody($header, $main, $footer);
-        include "Views/root.php";
-    }
 
-    /**
-     * @inheritDoc
-     */
-    public function post(array $args){
-        $head = $this->prepareHead("Shop");
-        $header = $this->prepareHeader_2();
-        $main = $this->prepareShop();
-        $footer = $this->prepareFooter();
-        $body = $this->prepareBody($header, $main, $footer);
-        var_dump($_POST);
-        include "Views/root.php";
-    }
+	/**
+	 * @inheritDoc
+	 */
+	public function get(array $args){
+		$root = get_text("root");
+		$head = $this->prepareHead("Shop");
+		$header = $this->prepareHeader_2($root['header']);
+		$main = $this->prepareShop();
+		$footer = $this->prepareFooter();
+		$body = $this->prepareBody($header, $main, $footer);
+		include "Views/root.php";
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function post(array $args){
+		$head = $this->prepareHead("Shop");
+		$header = $this->prepareHeader_2();
+		$main = $this->prepareShop();
+		$footer = $this->prepareFooter();
+		$body = $this->prepareBody($header, $main, $footer);
+		var_dump($_POST);
+		include "Views/root.php";
+	}
 }
