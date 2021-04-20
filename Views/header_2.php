68b98363657882f74d8dbe2f3cbf7bfecbd7602e
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-20 15:57:13 +0200

    sub header - 2

--- Views/header_2.php
+++ Views/header_2.php
@@ -2,6 +2,7 @@
 /**
  * @var array $text Contain all root text in the desired language
  */
+echo $title;
 ?>
 
 <nav id="header_2" class="navbar navbar-expand-lg py-4 py-lg-0 navbar-light bg-light">
