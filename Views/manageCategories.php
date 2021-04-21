19fb1a264781625db9c746ed06271fd6244c621a
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-21 14:08:22 +0200

    managecategories - mika

--- Views/manageCategories.php
+++ Views/manageCategories.php
@@ -1,1 +1,6 @@
-<?php
+<?php ?>
+<div id="div_main_manage_domains_caracteristiques" class="container-fluid d-flex flex-column mt-11 mt-lg-5">
+    <div id="div_manage_domain" class="row col-11 col-lg-4 border border-2 rounded-3 py-4 px-4 align-self-center">
+        <h1>Créer votre compte</h1>
+    </div>
+</div>
