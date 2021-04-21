76811919b0384a30ab9e3f9c15c9d6cbd6e0270c
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-22 01:36:13 +0200

    managecategories - mika

--- Views/manageCategories.php
+++ Views/manageCategories.php
@@ -191,14 +191,13 @@ modalAlterDomain.addEventListener('show.bs.modal', function (event) {
     // Button that triggered the modal
     var button = event.relatedTarget
   // Extract info from data-bs-* attributes
-  var recipient = button.getAttribute('data_domain_name')
-  var id_domain = button.getAttribute('data_domain_id')
+  var recipient = button.getAttribute('data-bs-whatever')
   // If necessary, you could initiate an AJAX request here
   // and then do the updating in a callback.
   //
   // Update the modal's content.
   var modalTitle = exampleModal.querySelector('.modal-title')
-  var modalBodyInput = exampleModal.exampleModal.querySelector('.modal-body input')
+  var modalBodyInput = exampleModal.querySelector('.modal-body input')
 
   modalTitle.textContent = 'New message to ' + recipient
   modalBodyInput.value = recipient
