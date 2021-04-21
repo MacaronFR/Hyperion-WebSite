3d498dc4c87320b8b94083a8829233fe1651e15e
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-21 16:07:23 +0200

    managecategories - mika

--- Views/manageCategories.php
+++ Views/manageCategories.php
@@ -1,7 +1,7 @@
 <?php ?>
 <div id="div_main_manage_domains_caracteristiques" class="container-fluid d-flex flex-column mt-11 mt-lg-2">
     <h1 style="text-align: center" class="mb-4">Gestion des caractéristiques & catégories</h1>
-    <div id="div_manage_domain" class="row col-11 col-lg-8 border border-2 border-orange-mine rounded-3 py-4 px-4 align-self-center divs_manage">
+    <div id="div_manage_domain" class="row col-11 col-lg-8 border border-2 border-orangemine rounded-3 py-4 px-4 align-self-center divs_manage">
         <div id="div_create_domain" class="container">
             <h3>Ajouter un domaine de produit</h3>
             <input class="form-control" type="text" placeholder="Saisie d'un domaine de produit">
