70336fa0acbbe06ad7f4b1cb4109a99b7cbd13b2
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-21 15:03:39 +0200

    managecategories - mika

--- Views/manageCategories.php
+++ Views/manageCategories.php
@@ -1,6 +1,6 @@
 <?php ?>
-<div id="div_main_manage_domains_caracteristiques" class="container-fluid d-flex flex-column mt-11 mt-lg-5">
-    <h1>Gestion des caractéristiques & catégories</h1>
+<div id="div_main_manage_domains_caracteristiques" class="container-fluid d-flex flex-column mt-11 mt-lg-2">
+    <h1 style="text-align: center">Gestion des caractéristiques & catégories</h1>
     <div id="div_manage_domain" class="row col-11 col-lg-4 border border-2 rounded-3 py-4 px-4 align-self-center">
         <div id="div_create_domain ">
             <h3>Ajouter un domaine de produit</h3>
@@ -57,5 +57,5 @@
                 </tbody>
             </table>
         </div>
-    </div>
+    </di>
 </div>
