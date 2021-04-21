9bb6ed8314e1a12d7140d100b2c0ab4dc6f536ba
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-21 14:22:43 +0200

    managecategories - mika

--- Views/manageCategories.php
+++ Views/manageCategories.php
@@ -1,6 +1,63 @@
 <?php ?>
 <div id="div_main_manage_domains_caracteristiques" class="container-fluid d-flex flex-column mt-11 mt-lg-5">
+    <h1>Gestion des caractéristiques & catégories</h1>
     <div id="div_manage_domain" class="row col-11 col-lg-4 border border-2 rounded-3 py-4 px-4 align-self-center">
-        <h1>Créer votre compte</h1>
+        <div id="div_create_domain ">
+            <h3>Ajouter un domaine de produit</h3>
+            <input class="form-control form-control-lg" type="text" placeholder=".form-control-lg" aria-label=".form-control-lg example">
+            <input class="form-control" type="text" placeholder="Default input" aria-label="default input example">
+            <input class="form-control form-control-sm" type="text" placeholder=".form-control-sm" aria-label=".form-control-sm example">
+        </div>
+        <div id="div_manage_all_domain">
+            <h3>Tous les domaines de produit</h3>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">Nom Du domaine</th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Téléphonie</td>
+                    <td><button type="button" class="btn btn-primary">Primary</button></td>
+                    <td><button type="button" class="btn btn-danger">Danger</button></td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Vidéo</td>
+                    <td><button type="button" class="btn btn-primary">Primary</button></td>
+                    <td><button type="button" class="btn btn-danger">Danger</button></td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Electro-ménager</td>
+                    <td><button type="button" class="btn btn-primary">Primary</button></td>
+                    <td><button type="button" class="btn btn-danger">Danger</button></td>
+                </tr>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Tv</td>
+                    <td><button type="button" class="btn btn-primary">Primary</button></td>
+                    <td><button type="button" class="btn btn-danger">Danger</button></td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Objets connectés</td>
+                    <td><button type="button" class="btn btn-primary">Primary</button></td>
+                    <td><button type="button" class="btn btn-danger">Danger</button></td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Audio & HIFI</td>
+                    <td><button type="button" class="btn btn-primary">Primary</button></td>
+                    <td><button type="button" class="btn btn-danger">Danger</button></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
