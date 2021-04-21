bf5da8ab3eb416965bf38311caff7e84a3dd1dc6
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-22 00:35:34 +0200

    managecategories - mika

--- Views/manageCategories.php
+++ Views/manageCategories.php
@@ -15,7 +15,7 @@
             </div>
         </div>
         <div id="div_manage_all_domain" class="container mt-3">
-            <h3>Tous les domaines de produit</h3>
+            <h3 class="mb-3">Tous les domaines de produit</h3>
             <div class="table-responsive">
                 <table class="table">
                     <thead>
@@ -72,7 +72,7 @@
     <!-- gestion types of product -->
     <div id="div_manage_domain" class="row col-11 col-lg-8 border border-2 border-warning rounded-3 py-4 px-4 align-self-center divs_manage mb-4">
         <div id="div_create_domain" class="container">
-            <h3>Ajouter un Type de produits associés à son domaine</h3>
+            <h3 class="mb-3">Ajouter un Type de produits associés à son domaine</h3>
             <div class="row">
                 <div class="col-5">
                     <input class="form-control" type="text" placeholder="Saisie d'un domaine de produit">
@@ -91,7 +91,7 @@
             </div>
         </div>
         <div id="div_manage_all_domain" class="container mt-3">
-            <h3>Tous les types de produits associés à leur domaine</h3>
+            <h3 class="mb-3">Tous les types de produits associés à leur domaine</h3>
             <div class="table-responsive">
                 <table class="table">
                     <thead>
