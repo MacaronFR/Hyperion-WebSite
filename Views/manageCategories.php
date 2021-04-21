90e6a6086fdb1cf14c34a034ed6842ce626304fa
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-21 16:38:18 +0200

    managecategories - mika

--- Views/manageCategories.php
+++ Views/manageCategories.php
@@ -5,7 +5,14 @@
     <div id="div_manage_domain" class="row col-11 col-lg-8 border border-2 border-warning rounded-3 py-4 px-4 align-self-center divs_manage mb-4">
         <div id="div_create_domain" class="container">
             <h3>Ajouter un domaine de produit</h3>
-            <input class="form-control" type="text" placeholder="Saisie d'un domaine de produit">
+            <div class="row">
+                <div class="col-7">
+                    <input class="form-control" type="text" placeholder="Saisie d'un domaine de produit">
+                </div>
+                <div class="col-3">
+                    <button class="btn btn-primary" type="submit">Ajouter</button>
+                </div>
+            </div>
         </div>
         <div id="div_manage_all_domain" class="container mt-3">
             <h3>Tous les domaines de produit</h3>
@@ -65,8 +72,23 @@
     <!-- gestion types of product -->
     <div id="div_manage_domain" class="row col-11 col-lg-8 border border-2 border-warning rounded-3 py-4 px-4 align-self-center divs_manage mb-4">
         <div id="div_create_domain" class="container">
-            <h3>Ajouter un Type de produits</h3>
-            <input class="form-control" type="text" placeholder="Saisie d'un domaine de produit">
+            <h3>Ajouter un Type de produits associés à son domaine</h3>
+            <div class="row">
+                <div class="col-5">
+                    <input class="form-control" type="text" placeholder="Saisie d'un domaine de produit">
+                </div>
+                <div class="col-5">
+                    <select class="form-select">
+                        <option selected>Choisir un Domaine de produit</option>
+                        <option value="1">Téléphonie</option>
+                        <option value="2">Vidéo</option>
+                        <option value="3">Electro-ménager</option>
+                    </select>
+                </div>
+                <div class="col-2">
+                    <button class="btn btn-primary" type="submit">Ajouter</button>
+                </div>
+            </div>
         </div>
         <div id="div_manage_all_domain" class="container mt-3">
             <h3>Tous les types de produits associés à leur domaine</h3>
