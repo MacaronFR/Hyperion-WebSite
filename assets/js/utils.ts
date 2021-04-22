2a99561ea594865f5d9dc2e632aa271db16e267d
MacaronFR <denisft77@gmail.com>
2021-04-22 18:46:17 +0200

    get ajax API

--- assets/js/utils.ts
+++ assets/js/utils.ts
@@ -1,15 +1,25 @@
 declare var $: any;
 declare var jquery: any;
 
-function API_REQUEST(path: string, method: string, param=[]){
+function API_REQUEST(path: string, method: string, param = []) {
 	let url = "https://api.hyperion.dev.macaron-dev.fr" + path;
-	$.ajax(url,
-		{
-			contentType: "application/json",
-			dataType: 'jsonp',
-			crossDomain: true,
-			method: method,
-			username: "hyperion",
-			password: "hyperiondeb1",
-		});
+	return new Promise<string>(function(resolve, reject){
+		var xhr = new XMLHttpRequest();
+		xhr.open(method, url, true);
+		xhr.onreadystatechange = function(){
+			if(xhr.readyState === xhr.DONE){
+				try {
+					let res = JSON.parse(xhr.responseText);
+					resolve(res);
+				}catch (e) {
+					reject(e);
+				}
+			}
+		}
+		if(method === "POST" || method === "PUT"){
+			xhr.send(JSON.stringify(param))
+		}else {
+			xhr.send();
+		}
+	})
 }
