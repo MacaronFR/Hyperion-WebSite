ec8200a4ef306b8a163af8835e0237cc094cacb0
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-22 02:45:23 +0200

    managecategories - mika

--- Views/manageCategories.php
+++ Views/manageCategories.php
@@ -151,6 +151,89 @@
             </div>
         </div>
     </div>
+
+    <!-- gestion specification of product -->
+    <div id="div_manage_types" class="row col-11 col-lg-8 border border-2 border-warning rounded-3 py-4 px-4 align-self-center divs_manage mb-4">
+        <div id="div_create_type" class="container mb-3">
+            <h3 class="mb-3">Ajouter un Type de produits associés à son domaine</h3>
+            <div class="row">
+                <div class="col-5">
+                    <input class="form-control" type="text" placeholder="Saisie d'un type de produit">
+                </div>
+                <div class="col-5">
+                    <select class="form-select">
+                        <option selected>Choisir un Domaine de produit</option>
+                        <option value="1">Téléphonie</option>
+                        <option value="2">Vidéo</option>
+                        <option value="3">Electro-ménager</option>
+                    </select>
+                </div>
+                <div class="col-2">
+                    <button class="btn btn-primary" type="submit">Ajouter</button>
+                </div>
+            </div>
+        </div>
+        <div id="div_manage_all_type" class="container mt-3">
+            <h3 class="mb-3">Tous les types de produits associés à leur domaine</h3>
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th scope="col">id</th>
+                        <th scope="col">Nom Du domaine</th>
+                        <th scope="col">Type de produit</th>
+                        <th scope="col"></th>
+                        <th scope="col"></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Téléphonie</td>
+                        <td>Smarthphone</td>
+                        <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalAlterType" data-type-id="25" data-type-domain-app="Objet connecté" data-type-name="Smartphone">Modifier</button></td>
+                        <td><button type="button" class="btn btn-danger">Supprimer</button></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Téléphonie</td>
+                        <td>Téléphone fix</td>
+                        <td><button type="button" class="btn btn-primary">Modifier</button></td>
+                        <td><button type="button" class="btn btn-danger">Supprimer</button></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Téléphonie</td>
+                        <td>Baby-phone</td>
+                        <td><button type="button" class="btn btn-primary">Modifier</button></td>
+                        <td><button type="button" class="btn btn-danger">Supprimer</button></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">4</th>
+                        <td>Tv</td>
+                        <td>écran plat</td>
+                        <td><button type="button" class="btn btn-primary">Modifier</button></td>
+                        <td><button type="button" class="btn btn-danger">Supprimer</button></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">5</th>
+                        <td>Objets connectés</td>
+                        <td>montre</td>
+                        <td><button type="button" class="btn btn-primary">Modifier</button></td>
+                        <td><button type="button" class="btn btn-danger">Supprimer</button></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">6</th>
+                        <td>Objets connectés</td>
+                        <td>HomePod</td>
+                        <td><button type="button" class="btn btn-primary">Modifier</button></td>
+                        <td><button type="button" class="btn btn-danger">Supprimer</button></td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
 
 
