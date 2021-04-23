86a71747a1f648f1d4d938a377087b7aca69d0e2
CL4P-TR4P <mika-xuan@hotmail.com>
2021-04-23 04:26:52 +0200

    manageallProduct - mika

--- Views/manageAllProducts.php
+++ Views/manageAllProducts.php
@@ -1,6 +1,6 @@
 <div id="div_main_manage_domains_caracteristiques" class="container-fluid d-flex flex-column mt-11 mt-lg-2">
     <h1 style="text-align: center" class="mb-4">Gestion de l'ensemble des produits</h1>
-    <!-- gestion categories of product -->
+    <!--
     <div id="div_manage_domain" class="row col-11 col-lg-8 border border-2 border-warning rounded-3 py-4 px-4 align-self-center divs_manage mb-4">
         <div id="div_manage_all_products" class="container mt-3">
             <h3 class="mb-3">Tous les produits et leurs specs</h3>
@@ -62,5 +62,5 @@
                 </table>
             </div>
         </div>
-    </div>
+    </div> -->
 </div>
