689d01c5f3fcaa2f6dc571a23c620d584ad9356c
CL4P-TR4P <mika-xuan@hotmail.com>
2021-05-03 20:05:32 +0200

    manageAllReference.php - mika

--- Views/header_2.php
+++ Views/header_2.php
@@ -116,4 +116,21 @@
         </div>
     </div>
 </nav>
+<?php elseif ($title === "Manage Categories"): ?>
+    <nav class="navbar navbar-expand-lg navbar-dark subHeader3">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Catégories</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="/manageAddReference">Ajouter une offre</a>
+                    <a class="nav-link" href="/manageAllReferences">Consulter mes offres en attentes</a>
+                    <a class="nav-link" href="/manageAllProducts">Consulter l'historique des offres</a>
+                </div>
+            </div>
+        </div>
+    </nav>
 <?php endif; ?>
+
