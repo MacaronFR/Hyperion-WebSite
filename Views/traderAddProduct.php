642597abf12a6845a68ec77fcea41f2421a60317
CL4P-TR4P <mika-xuan@hotmail.com>
2021-05-03 23:43:55 +0200

    traderAddProduct - mika

--- Views/traderAddProduct.php
+++ Views/traderAddProduct.php
@@ -3,7 +3,7 @@
 <div id="div_trader_add_offer_general" class="container-fluid d-flex flex-column">
     <h1 class="align-self-center mt-4">Créer une offre</h1>
     <div class="d-flex flex-column flex-lg-row mt-11 mt-lg-5 justify-content-end pe-lg-5">
-        <div id="div_trader_add_offer" class="row col-11 col-lg-5 border border-2 border-orange rounded-3 position me-lg-5 px-5">
+        <div id="div_trader_add_offer" class="row col-11 col-lg-5 border border-2 border-warning rounded-3 position me-lg-5 px-5">
             <form action="#" method="post">
                 <div class="form-group mt-1 mt-lg-4">
                     <div class="mx-2">
