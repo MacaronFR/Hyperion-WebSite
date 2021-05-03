9a2e1530f219a41e99709f31a7e9ef552b9b5c5e
CL4P-TR4P <mika-xuan@hotmail.com>
2021-05-03 23:56:44 +0200

    traderAddProduct - mika

--- Views/traderAddProduct.php
+++ Views/traderAddProduct.php
@@ -2,7 +2,7 @@
 ?>
 <div id="div_trader_add_offer_general" class="container-fluid d-flex flex-column">
     <h1 class="align-self-center mt-4">Créer une offre</h1>
-    <div class="d-flex flex-column flex-lg-row mt-11 mt-lg-4 justify-content-end pe-lg-5">
+    <div class="d-flex flex-column flex-lg-row mt-11 mt-lg-4 justify-content-end pe-lg-5 mb-5">
         <div id="div_trader_add_offer" class="row col-11 col-lg-5 border border-2 border-warning rounded-3 position me-lg-5 px-5 py-3">
             <form action="#" method="post">
                 <div class="form-group mt-1 mt-lg-4">
@@ -75,7 +75,7 @@
                 </div>
             </form>
         </div>
-        <div id="div_trader_add_offer_price" class="row col-11 col-lg-3 me-lg-5 ms-lg-5 px-4 mb-5">
+        <div id="div_trader_add_offer_price" class="row col-11 col-lg-3 me-lg-5 ms-lg-5 px-4">
             <h3>Estimation du prix d'achat</h3>
             <div id="priceEstimationOk">
                 <p>Indisponible</p>
