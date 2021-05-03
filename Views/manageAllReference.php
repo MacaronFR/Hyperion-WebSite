0234c79621a270ddfb00700dd2d0545974717cf4
CL4P-TR4P <mika-xuan@hotmail.com>
2021-05-03 19:20:47 +0200

    manageAllReference.php - mika

--- Views/manageAllReference.php
+++ Views/manageAllReference.php
@@ -3,62 +3,34 @@
     <div id="div_manage_domain" class="row col-11 col-lg-8 border border-2 border-warning rounded-3 py-4 px-4 align-self-center divs_manage mb-4">
         <div id="div_manage_all_products" class="container mt-3">
             <h3 class="mb-3">Tous les produits et leurs specs</h3>
-            <div class="table-responsive">
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th scope="col">id</th>
-                        <th scope="col">Nom Du produit</th>
-                        <th scope="col">Specs du produit</th>
-                        <th scope="col"></th>
-                        <th scope="col"></th>
-                    </tr>
-                    </thead>
-                    <tbody id="tabCat">
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>iphone 6s</td>
-                        <td>16 GB</td>
-                        <td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalAlterType" data-type-id="25" data-type-domain-app="Objet connecté" data-type-name="Smartphone">Modifier</button></td>
-                        <td><button type="button" class="btn btn-danger">Supprimer</button></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>iphone 6s</td>
-                        <td>32 GB</td>
-                        <td><button type="button" class="btn btn-primary">Modifier</button></td>
-                        <td><button type="button" class="btn btn-danger">Supprimer</button></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>iphone 6s</td>
-                        <td>64 GB</td>
-                        <td><button type="button" class="btn btn-primary">Modifier</button></td>
-                        <td><button type="button" class="btn btn-danger">Supprimer</button></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">4</th>
-                        <td>iphone 6s</td>
-                        <td>Noir</td>
-                        <td><button type="button" class="btn btn-primary">Modifier</button></td>
-                        <td><button type="button" class="btn btn-danger">Supprimer</button></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">5</th>
-                        <td>iphone 6s</td>
-                        <td>Blanc</td>
-                        <td><button type="button" class="btn btn-primary">Modifier</button></td>
-                        <td><button type="button" class="btn btn-danger">Supprimer</button></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">6</th>
-                        <td>Samsung A6</td>
-                        <td>32 GB</td>
-                        <td><button type="button" class="btn btn-primary">Modifier</button></td>
-                        <td><button type="button" class="btn btn-danger">Supprimer</button></td>
-                    </tr>
-                    </tbody>
-                </table>
+            <div id="div_manage_all_category" class="container mt-3">
+                <h3 class="mb-3">Toutes les catégories de produit</h3>
+                <div class="table-responsive">
+                    <table
+                            class="table"
+                            id="table_categories"
+                            data-toggle="table"
+                            data-search="true"
+                            data-pagination="true"
+                            data-height="600"
+                            data-ajax="..."
+                            data-side-pagination="server"
+                            data-row-attributes="rowAttributes">
+                        <thead>
+                        <tr>
+                            <th data-sortable="true" data-field="id">id</th>
+                            <th data-sortable="true" data-field="name">Catégories</th>
+                            <th data-sortable="true" data-field="name">Types</th>
+                            <th data-sortable="true" data-field="name">Marques</th>
+                            <th data-sortable="true" data-field="name">Modeles</th>
+                            <th data-sortable="true" data-field="name">Prix d'achat</th>
+                            <th data-sortable="true" data-field="name">Prix de vente</th>
+                            <th data-field="modif"></th>
+                            <th data-field="suppr"></th>
+                        </tr>
+                        </thead>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
