732a2e94d274cac57d2f88041869d5a30dec31ef
MacaronFR <denisft77@gmail.com>
2021-05-13 00:34:11 +0200

    bootstrap 5

--- Views/head.php
+++ Views/head.php
@@ -11,8 +11,8 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <link href="/assets/css/main.css" type="text/css" rel="stylesheet">
-<link href="/assets/bootstrap-5/css/bootstrap.min.css" type="text/css" rel="stylesheet">
-<script src="/assets/bootstrap-5/js/bootstrap.min.js"></script>
+<link href="/assets/bootstrap-5/dist/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+<script src="/assets/bootstrap-5/dist/js/bootstrap.min.js"></script>
 <link rel="icon" href="/assets/images/Hyperion-yellow-transparent.png">
 <link href="/assets/WillowBody_pc_ps3.ttf">
 <meta name="viewport" content="width=device-width, initial-scale=1">
