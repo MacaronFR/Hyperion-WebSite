a9dbadc7d355946093437810dfe72d827297afe5
CL4P-TR4P <mika-xuan@hotmail.com>
2021-05-16 14:21:14 +0200

    traderpendingproduct - mika

--- Views/traderPendingProduct.php
+++ Views/traderPendingProduct.php
@@ -84,7 +84,7 @@
                 </tbody>
             </table>
         </div>
-        <div id="divtraderpendinginfo" class="container-fluid col-lg-3 d-flex flex-column justify-content-center my-auto py-3 border border-2 border-warning rounded-3">
+        <div id="divtraderpendinginfo" class="container-fluid col-lg-3 d-flex flex-column justify-content-center my-auto p-4 border border-2 border-warning rounded-3">
             <h4 class="align-self-center mb-3">informations du produit</h4>
             <input type="text" class="form-control mb-1" value="Smartphone" style="text-align: center">
             <input type="text" class="form-control mb-1" value="Apple" style="text-align: center">
