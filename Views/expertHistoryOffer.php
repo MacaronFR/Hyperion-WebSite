e4bf206a148e14d3a3622bded006495fc85d8950
CL4P-TR4P <mika-xuan@hotmail.com>
2021-05-18 00:43:01 +0200

    add expert history offer -  mika

--- Views/expertHistoryOffer.php
+++ Views/expertHistoryOffer.php
@@ -1,1 +1,84 @@
-<?php
+<div id="experthistoryoffergeneral" class="container-fluid d-flex flex-column">
+    <h3 class="align-self-center mb-4 mt-3">Offres non traitées</h3>
+    <div class="container-fluid d-flex flex-column flex-lg-row">
+        <div id="divexperthistoryoffertable" class="container-fluid col-lg-7">
+            <table class="table"
+                   id="table_spec"
+                   data-toggle="table"
+                   data-pagination="true"
+                   data-height="600">
+                <thead>
+                <tr>
+                    <th data-sortable data-field="id">id de l'offre</th>
+                    <th data-sortable data-field="type">type de produit</th>
+                    <th data-sortable data-field="state">date de fin de l'offre</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">2134214</th>
+                    <td>Smartphone</td>
+                    <td>17/05/2021</td>
+                </tr>
+                <tr>
+                    <th scope="row">2134214</th>
+                    <td>Smartphone</td>
+                    <td>17/05/2021</td>
+                </tr>
+                <tr>
+                    <th scope="row">2134214</th>
+                    <td>Smartphone</td>
+                    <td>17/05/2021</td>
+                </tr>
+                <tr>
+                    <th scope="row">2134214</th>
+                    <td>Smartphone</td>
+                    <td>17/05/2021</td>
+                </tr>
+                <tr>
+                    <th scope="row">2134214</th>
+                    <td>Smartphone</td>
+                    <td>17/05/2021</td>
+                </tr>
+                <tr>
+                    <th scope="row">2134214</th>
+                    <td>Smartphone</td>
+                    <td>17/05/2021</td>
+                </tr>
+                <tr>
+                    <th scope="row">2134214</th>
+                    <td>Smartphone</td>
+                    <td>17/05/2021</td>
+                </tr>
+                <tr>
+                    <th scope="row">2134214</th>
+                    <td>Smartphone</td>
+                    <td>17/05/2021</td>
+                </tr>
+                <tr>
+                    <th scope="row">2134214</th>
+                    <td>Smartphone</td>
+                    <td>17/05/2021</td>
+                </tr>
+                <tr>
+                    <th scope="row">2134214</th>
+                    <td>Smartphone</td>
+                    <td>17/05/2021</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div id="divexperthistoryofferinfo" class="container-fluid col-lg-3 d-flex flex-column justify-content-center my-auto p-4 border border-2 border-warning rounded-3">
+            <h4 class="align-self-center mb-3">informations du produit</h4>
+            <input type="text" class="form-control mb-1" value="Smartphone" style="text-align: center">
+            <input type="text" class="form-control mb-1" value="Apple" style="text-align: center">
+            <input type="text" class="form-control mb-1" value="6s - White" style="text-align: center">
+            <input type="text" class="form-control mb-1" value="16 Go" style="text-align: center">
+            <input type="text" class="form-control mb-1" value="Correct" style="text-align: center">
+            <input type="text" class="form-control mb-1" value="145 €" style="text-align: center">
+            <div class="d-grid mt-3">
+                <button type="button" class="btn btn-warning me-1">traiter cette offre</button>
+            </div>
+        </div>
+    </div>
+</div>
