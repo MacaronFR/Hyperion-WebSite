68c3fac750450bca3b248c01b41c9aec121e78a0
CL4P-TR4P <mika-xuan@hotmail.com>
2021-05-21 14:40:11 +0200

    expert consult one offer - mika

--- Views/expertConsultOneOffer.php
+++ Views/expertConsultOneOffer.php
@@ -1,1 +1,56 @@
-<?php
+<div id="expertoneoffergeneral" class="container-fluid d-flex flex-column">
+    <h3 class="align-self-center mb-4 mt-3">Gestion de l'offre</h3>
+    <div class="container-fluid d-flex flex-column flex-lg-row">
+        <div class="container-fluid d-flex flex-column col-lg-5">
+            <div id="divexperthistoryofferinfo" class="container-fluid d-flex flex-column justify-content-center my-auto p-4 border border-2 border-warning rounded-3 mb-3">
+                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="cl4p-tp_right.png" class="d-block w-100">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="cl4p-tp_center.png" class="d-block w-100">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="cl4p-tp_left.png" class="d-block w-100">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+                <div class="d-grid mt-3">
+                    <button type="button" class="btn btn-warning me-1">Consulter l'offre</button>
+                </div>
+            </div>
+            <div id="divexperthistoryoffertable" class="container-fluid my-auto p-4 border border-2 border-warning rounded-3">
+                <div class="">
+                    <label for="description" class="form-label">Description du produit</label>
+                    <textarea class="form-control" id="description" rows="3"></textarea>
+                </div>
+            </div>
+        </div>
+        <div id="divexperthistoryofferinfo" class="container-fluid col-lg-4 d-flex flex-column justify-content-center my-auto p-4 border border-2 border-warning rounded-3">
+            <h4 class="align-self-center mb-3">informations du produit</h4>
+            <input type="text" class="form-control mb-1" value="Smartphone" style="text-align: center">
+            <input type="text" class="form-control mb-1" value="Apple" style="text-align: center">
+            <input type="text" class="form-control mb-1" value="6s - White" style="text-align: center">
+            <input type="text" class="form-control mb-1" value="16 Go" style="text-align: center">
+            <input type="text" class="form-control mb-1" value="Correct" style="text-align: center">
+            <input type="text" class="form-control mb-1" value="145 €" style="text-align: center">
+            <div class="d-grid mt-3">
+                <button type="button" class="btn btn-warning me-1">Offre acceptée</button>
+            </div>
+        </div>
+    </div>
+</div>
