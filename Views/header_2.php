abfb2313a02d23167287458001fd7336481ed6bd
CL4P-TR4P <mika-xuan@hotmail.com>
2021-05-27 14:36:08 +0200

    header  update - mika

--- Views/header_2.php
+++ Views/header_2.php
@@ -56,15 +56,12 @@
         <div class="collapse navbar-collapse" id="navBarCategories">
             <div class="navbar-nav">
                 <a class="nav-link active" aria-current="page" href="#"><?=$text['header']['products']?></a>
-                <a class="nav-link" href="#"><?=$text['header']['project']['human']?></a>
-                <a class="nav-link disabled" href="#" ><?=$text['header']['project']['eco']?></a>
                 <?php if(isset($_SESSION['level']) AND $_SESSION['level']==='3'): ?>
                     <a class="nav-item nav-link" href="/trader/add/offer"><?=$text['header']['trader']['offer']?></a>
                 <?php endif; ?>
                 <?php if(isset($_SESSION['level']) AND $_SESSION['level']<'3'): ?>
                     <a class="nav-item nav-link" href="/manage/add/reference"><?=$text['header']['staff']['product']?></a>
                     <a class="nav-item nav-link" href="/expert/offers"><?=$text['header']['staff']['offer']?></a>
-                    <a class="nav-item nav-link" href="#"><?=$text['header']['staff']['project']?></a>
                 <?php endif; ?>
                 <?php if(isset($_SESSION['level']) AND $_SESSION['level']<'2'): ?>
                     <a class="nav-item nav-link" href="#"><?=$text['header']['admin']['admin']?></a>
