8cf93f8a9f514d9cc605951f6c207cd530e3b7e1
CL4P-TR4P <mika-xuan@hotmail.com>
2021-06-03 22:21:55 +0200

    shop one prod update - mika

---
+++ Views/shopOneProduct.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Factory</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.css">
+    <script src="https://unpkg.com/bootstrap-table@1.18.3/dist/bootstrap-table.min.js"></script>
+
+</head>
+<body>
+<div id="expertoneoffergeneral" class="container-fluid d-flex flex-column">
+    <h3 class="align-self-center mb-4 mt-3">Offre no 8967856</h3>
+    <div class="container-fluid d-flex flex-column flex-lg-row mb-lg-5 mb-3">
+        <div class="container-fluid d-flex flex-column col-lg-5 p-0 mb-3">
+            <div id="divexpertoneoffer" class="container-fluid d-flex flex-column justify-content-center my-auto p-4 border border-2 border-warning rounded-3 mb-3">
+                <div id="carouselexpertoneoffer" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <div class="w-100 d-flex justify-content-center">
+                                <img src="assets/images/cl4p-tp_right.png" class="align-self-center">
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="w-100 d-flex justify-content-center">
+                                <img src="assets/images/cl4p-tp_center.png" class="align-self-center">
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <div class="w-100 d-flex justify-content-center">
+                                <img src="assets/images/cl4p-tp_left.png" class="align-self-center">
+                            </div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselexpertoneoffer" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselexpertoneoffer" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+            <div id="divexperthistoryoffertable" class="container-fluid my-auto p-4 border border-2 border-warning rounded-3">
+                <div class="">
+                    <label for="description" class="form-label">Description du produit</label>
+                    <textarea class="form-control" id="description" rows="3"></textarea>
+                </div>
+            </div>
+        </div>
+        <div id="divexperthistoryofferinfo" class="container-fluid col-lg-4 d-flex flex-column justify-content-center my-auto p-4 border border-2 border-warning rounded-3">
+            <h4 class="align-self-center mb-3">informations du produit</h4>
+            <input type="text" class="form-control mb-1" value="Smartphone" style="text-align: center">
+            <input type="text" class="form-control mb-1" value="Apple" style="text-align: center">
+            <input type="text" class="form-control mb-1" value="6s - White" style="text-align: center">
+            <input type="text" class="form-control mb-1" value="16 Go" style="text-align: center">
+            <input type="text" class="form-control mb-1" value="Correct" style="text-align: center">
+            <input type="text" class="form-control mb-1" value="145 €" style="text-align: center">
+
+            <div class="d-grid mt-3">
+                <button type="button" class="btn btn-success me-1">Ajouter au panier</button>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
