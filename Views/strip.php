0dec39283889e434220e035c34497a78f0e009ff
CL4P-TR4P <mika-xuan@hotmail.com>
2021-06-05 18:27:29 +0200

    update - mika

--- Views/strip.php
+++ Views/strip.php
@@ -1,1 +1,13 @@
-
+<form action="pay.php" method="POST">
+    <script
+        src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+        data-key="clée_api_strip"
+        data-amount="469"
+        data-name="Hyperion"
+        data-description="Smartphone"
+        data-image="/assets/images/cl4p-tp_center.png"
+        data-locale="auto"
+        data-currency="eur"
+        data-label="Payer avec Strip" >
+    </script>
+</form>
