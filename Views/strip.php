34c48d95d4bf6d9c4a783c24087f80e0eacc9db5
CL4P-TR4P <mika-xuan@hotmail.com>
2021-06-05 18:41:50 +0200

    update - mika

--- Views/strip.php
+++ Views/strip.php
@@ -1,7 +1,7 @@
 <form action="pay.php" method="POST">
     <script
         src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-        data-key="clée_api_strip"
+        data-key="sk_test_51IyypYGc9T6D4Xa9C5Y2Ypke3QmSsyKX2N1nCjgLIAXqpZVFQZ4AR9FHBHjGHxmy4eI5OQzt8K2NTql8wIgC4Q0c00vnBLSKZx"
         data-amount="469"
         data-name="Hyperion"
         data-description="Smartphone"
