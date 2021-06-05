2ceefa136b7406e21402b72bb03cf5f51698b7ec
CL4P-TR4P <mika-xuan@hotmail.com>
2021-06-05 18:48:54 +0200

    update - mika

--- Views/cart.php
+++ Views/cart.php
@@ -35,7 +35,7 @@
             </div>
             <form action="pay.php" method="POST">
                 <script
-                        src="https://checkout.stripe.com/checkout.js" class="stripe-button btn btn-primary col-12"
+                        src="https://checkout.stripe.com/checkout.js" class="btn btn-primary col-12"
                         data-key="pk_test_51IyypYGc9T6D4Xa9gQX722Yl0Km8pX2NzxMXGaovV0I1SXefG4LyT0Gtr1RBjB6CO0HWLyMW9bNGLQkKy4GyeVFV00Bs9RVSq1"
                         data-amount="469"
                         data-name="Hyperion"
