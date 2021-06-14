226187c1055e9700e3ec443ca7db236408191f69
CL4P-TR4P <mika-xuan@hotmail.com>
2021-06-14 23:08:31 +0200

    update - mika

--- Views/traderInstruction.php
+++ Views/traderInstruction.php
@@ -1,6 +1,6 @@
 <div class="d-flex flex-column">
     <div class="mt-5 d-flex flex-column">
-        <h1 class="d-flex justify-content-center mb-4 mt-5">Confirmation de votre offre</h1>
+        <h1 class="d-flex justify-content-center mb-4">Confirmation de votre offre</h1>
         <div class="d-flex flex-column p-3 border border-2 rounded-3 col-11 col-lg-9 align-self-center">
             <h3>Nous vous remercions d'avoir fait appel à nos services. </h3>
             <p> un bon colissimo vous a été envoyé par courrier, veuillez vous en servir pour nous envoyer votre produit. </p>
