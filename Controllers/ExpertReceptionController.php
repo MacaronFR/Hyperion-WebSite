392bf80a4d96ac1ba07207b768673ee2677bd7d0
CL4P-TR4P <mika-xuan@hotmail.com>
2021-06-23 21:29:13 +0200

    /expert/reception - mika

---
+++ Controllers/ExpertReceptionController.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Hyperion\WebSite;
+
+
+class ExpertReceptionController extends Controller
+{
+
+    protected function ExpertReception(): string{
+        ob_start();
+        include "Views/expertReception.php";
+        return ob_get_clean();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function get(array $args){
+        $root = get_text("root");
+        $head = $this->prepareHead("Expert Reception");
+        $header = $this->prepareHeader_2($root['header'], "Expert: Reception");
+        $main = $this->ExpertReception();
+        $footer = $this->prepareFooter();
+        $body = $this->prepareBody($header, $main, $footer);
+        include "Views/root.php";
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function post(array $args)
+    {
+        return false;
+    }
+}
