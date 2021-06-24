d49e98102b896a5a8e9fc4d8d59c459f6beba245
CL4P-TR4P <mika-xuan@hotmail.com>
2021-06-24 23:31:58 +0200

    update - mika

--- Views/payement_accepted.php
+++ Views/payement_accepted.php
@@ -4,8 +4,8 @@
  */
 ?>
 <div class="d-flex flex-column">
-    <h1>Paiment Accepter</h1>
-    <div class="col-12 col-lg-6">
+    <h1 class="align-self-center">Paiment Accepter</h1>
+    <div class="col-12 col-lg-6 align-self-center">
         <object type="<?= $invoice['file']['type']?>" data="data:<?= $invoice['file']['type']?>;base64,<?= $invoice['file']['content']?>" style="height: 90vh; width: 100%" ></object>
     </div>
 </div>
