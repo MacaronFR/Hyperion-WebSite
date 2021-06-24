45cf3b04d2659790315b0d3a5c2e31d6e3ce19c9
MacaronFR <denisft77@gmail.com>
2021-06-24 23:47:19 +0200

    invoice + credits one

--- Views/administrationFactures.php
+++ Views/administrationFactures.php
@@ -1,35 +1,7 @@
 <div class="container-fluid col-10 d-flex flex-column">
     <div id="AdministrationFacturesClients" class="d-flex flex-column">
         <h3 class="align-self-center mb-4 mt-3">Gestion des factures</h3>
-        <div class="container-fluid d-flex flex-column border border-2 py-4 rounded-2 border-warning mb-4">
-            <h3 class="align-self-center mb-3">Factures Générales</h3>
-            <div class="mb-3">
-                <label for="cible" class="form-label">Cible(s)</label>
-                <select class="form-select" aria-label="Default select example" id="cible">
-                    <option selected>Selection Cible(s)</option>
-                    <option value="1">Clients</option>
-                    <option value="2">Marchands</option>
-                    <option value="3">Clients + Marchands</option>
-                </select>
-            </div>
-            <div class="mb-3">
-                <label for="cible" class="form-label">Type de Transaction</label>
-                <select class="form-select" aria-label="Default select example" id="cible">
-                    <option selected>Selection transaction</option>
-                    <option value="1">Achats</option>
-                    <option value="2">Ventes</option>
-                    <option value="3">Achats + Ventes</option>
-                </select>
-            </div>
-            <div class="mb-3">
-                <label for="inputDate" class="form-label">Selection période</label>
-                <input type="date" class="form-control" id="inputDate">
-            </div>
-            <div class="mb-3">
-                <button type="button" class="btn btn-success col-12" id="billExportGlobal">Export CSV</button>
-            </div>
-        </div>
-        <div class="container-fluid d-flex flex-column border border-2 py-4 rounded-2 border-warning mb-4">
+        <div class="container-fluid d-flex flex-column border border-2 py-4 rounded-2 border-primary mb-4">
             <h3 class="align-self-center mb-3">Factures clients (détails)</h3>
             <div class="d-flex flex-column flex-lg-row">
                 <div id="divexpertoffertable" class="container-fluid col-lg-7">
@@ -87,7 +59,7 @@
                         </tbody>
                     </table>
                 </div>
-                <div id="divexpertofferinfo" class="container-fluid col-lg-3 d-flex flex-column justify-content-center my-auto p-4 border border-2 border-warning rounded-3">
+                <div id="divexpertofferinfo" class="container-fluid col-lg-3 d-flex flex-column justify-content-center my-auto p-4 border border-2 border-primary rounded-3">
                     <h4 class="align-self-center mb-3">informations Facture</h4>
                     <input type="text" class="form-control mb-1" value="TALLA Michael" style="text-align: center">
                     <input type="text" class="form-control mb-1" value="1 rue de Strasbourg" style="text-align: center">
@@ -99,7 +71,7 @@
                 </div>
             </div>
         </div>
-        <div class="container-fluid d-flex flex-column border border-2 py-4 rounded-2 border-warning mb-4">
+        <div class="container-fluid d-flex flex-column border border-2 py-4 rounded-2 border-primary mb-4">
             <h3 class="align-self-center mb-3">Factures Marchands (détails)</h3>
             <div class="d-flex flex-column flex-lg-row">
                 <div id="divexpertoffertable" class="container-fluid col-lg-7">
@@ -139,7 +111,7 @@
                         </tbody>
                     </table>
                 </div>
-                <div id="divexpertofferinfo" class="container-fluid col-lg-3 d-flex flex-column justify-content-center my-auto p-4 border border-2 border-warning rounded-3">
+                <div id="divexpertofferinfo" class="container-fluid col-lg-3 d-flex flex-column justify-content-center my-auto p-4 border border-2 border-primary rounded-3">
                     <h4 class="align-self-center mb-3">informations Facture</h4>
                     <input type="text" class="form-control mb-1" value="TALLA Michael" style="text-align: center">
                     <input type="text" class="form-control mb-1" value="1 rue de Strasbourg" style="text-align: center">
