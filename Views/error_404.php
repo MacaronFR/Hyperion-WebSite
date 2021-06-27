8e24a8add343dd7b70e38e0fee1c829a97fba5d9
CL4P-TR4P <mika-xuan@hotmail.com>
2021-06-27 13:03:29 +0200

    Update 403-400-404-500 - mika

--- Views/error_404.php
+++ Views/error_404.php
@@ -1,1 +1,6 @@
-<?php
+<div class="container-fluid m-0 p-0 col-12" style="height: 100vh;">
+    <img src="../assets/images/imgClapi3.jpg" style="width: 100%; height: 100%">
+    <div class="Centered" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+        <h1 style="color: lightgray">Error 404</h1>
+        <h1 style="color: lightgray">Not Found</h1>
+    </div>
--- Views/error_500.php
+++ Views/error_500.php
@@ -1,1 +1,6 @@
-<?php
+<div class="container-fluid m-0 p-0 col-12" style="height: 100vh;">
+    <img src="../assets/images/imgClapi1.png" style="width: 100%; height: 100%">
+    <div class="Centered" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
+        <h1>Error 500</h1>
+        <h1>Internal Server Error</h1>
+    </div>
