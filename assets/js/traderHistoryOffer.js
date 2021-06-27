6f0cbce3085cd7718c84c1ebe3e6528fee27db21
Retr0 <ngallot@myges.fr>
2021-06-27 15:37:31 +0200

    traderHistoryOffer

--- assets/js/traderHistoryOffer.js
+++ assets/js/traderHistoryOffer.js
@@ -4,32 +4,84 @@ var toastEL = [].slice.call(document.querySelectorAll(".toast"));
 var toastList = toastEL.map(function (toastE) {
     return new bootstrap.Toast(toastE);
 });
-function retrieve_history(params) {
-    var url = prepare_url(params, "/offer/history/" + token + "/");
+function retrieve_pending(params) {
+    var url = "/offer/pending/";
+    url += token + "/";
+    url += params.data.offset / 10;
     API_REQUEST(url, "GET").then(function (res) {
-        console.log(res);
-        var rows = [];
-        var total = res['content'].total;
-        var totalNotFiltered = res['content'].totalNotFiltered;
-        delete res['content']['total'];
-        delete res['content']['totalNotFiltered'];
-        for (var i = 0; i < Object.keys(res.content).length; ++i) {
-            rows.push(res.content[i]);
+        if (res.status.code === 200) {
+            var rows = [];
+            var total = res['content'].total;
+            var totalNotFiltered = res['content'].totalNotFiltered;
+            delete res['content']['total'];
+            delete res['content']['totalNotFiltered'];
+            for (var i = 0; i < Object.keys(res.content).length; ++i) {
+                rows.push(res.content[i]);
+                var color = (res.content[i]['status'] == 5) ? "primary" : "secondary";
+                var buttonText = res.content[i]['status'] == 5 ? "accepted" : "refused";
+                rows[i]['status'] = text['status'][rows[i]['status']];
+                rows[i]['state'] = text['state'][rows[i]['state']];
+                rows[i]['detail'] = "<button type=\"button\" class=\"btn btn-" + color + "\" data-offer-id=\"" + rows[i]['id'] + "\" data-offer-type=\"" + rows[i]['type'] + "\" data-offer-brand=\"" + rows[i]['brand'] + "\" data-offer-model=\"" + rows[i]['model'] + "\" data-offer-state=\"" + rows[i]['state'] + "\" data-offer=\"" + rows[i]['offer'] + "\" data-offer-counter=\"" + rows[i]['counter_offer'] + "\" onclick=\"seeDetail(this)\">" + text[buttonText] + "</button>";
+            }
+            params.success({ "total": total, "totalNotFiltered": totalNotFiltered, "rows": rows });
         }
-        params.success({ "total": total, "totalNotFiltered": totalNotFiltered, "rows": rows });
+        else if (res.status.code === 204) {
+            $("#ToastWarning").children(".toast-body").text("Vous n'avez pas d'offre ");
+            toastList[2].show();
+            params.error();
+        }
     });
 }
-var button_history = "<div class=\"d-flex justify-content-center mt-3 button-detail\">" +
-    "<button type=\"button\" class=\"btn btn-success me-1 col-6 accept\">Accepter</button>" +
-    "<button type=\"button\" class=\"btn btn-danger ms-1 col-6 refuse\">Refuser</button>" +
+var buttonHistory = "<div class=\"d-flex justify-content-center mt-3 button-detail\">" +
+    "<button type=\"button\" class=\"btn btn-success me-1 col-6 accepted\">Accepter !</button>" +
+    "<button type=\"button\" class=\"btn btn-danger ms-1 col-6 refused\">Refuser !</button>" +
     "</div>";
 function seeDetail(element) {
-    var detail = $("#divtraderhistoryinfo");
+    var detail = $("#divtraderpendinginfo");
     detail.find(".button-detail").remove();
     detail.find("[name=type]").val(element.dataset['offerType']);
     detail.find("[name=brand]").val(element.dataset['offerBrand']);
     detail.find("[name=model]").val(element.dataset['offerModel']);
     detail.find("[name=state]").val(element.dataset['offerState']);
     detail.find("[name=offer]").val(element.dataset['offer'] + " €");
+    if (element.dataset['offerCounter'] === "null") {
+        detail.find("[name=counter]").val("").attr("disabled", true);
+    }
+    else {
+        detail.find("[name=counter]").val(element.dataset['offerCounter'] + " €").removeAttr("disabled");
+        detail.append(buttonHistory);
+        detail.find(".accept").off("click").on("click", function () {
+            API_REQUEST("/offer/counter/accept/" + token + "/" + element.dataset['offerId'], "PUT").then(function (res) {
+                if (res.status.code === 200) {
+                    $(".table").bootstrapTable("refresh");
+                    $("#ToastSuccess").children(".toast-body").text("Offre Acceptée");
+                    toastList[1].show();
+                }
+                else {
+                    $("#ToastWarning").children(".toast-body").text("Erreur");
+                    toastList[1].show();
+                }
+            }).catch(function () {
+                $("#ToastError").children(".toast-body").text("Erreur");
+                toastList[0].show();
+            });
+        });
+        detail.find(".refuse").off("click").on("click", function () {
+            API_REQUEST("/offer/counter/refuse/" + token + "/" + element.dataset['offerId'], "PUT").then(function (res) {
+                if (res.status.code === 200) {
+                    $(".table").bootstrapTable("refresh");
+                    $("#ToastWarning").children(".toast-body").text("Offre Refusée");
+                    toastList[2].show();
+                }
+                else {
+                    $("#ToastWarning").children(".toast-body").text("Erreur");
+                    toastList[1].show();
+                }
+            }).catch(function () {
+                $("#ToastError").children(".toast-body").text("Erreur");
+                toastList[0].show();
+            });
+        });
+    }
 }
 //# sourceMappingURL=traderHistoryOffer.js.map
