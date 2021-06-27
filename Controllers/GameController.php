2fd16aca9598a0678437efa8c28da8258aab336e
CL4P-TR4P <mika-xuan@hotmail.com>
2021-06-27 17:34:13 +0200

    update - mika

--- Controllers/GameController.php
+++ Controllers/GameController.php
@@ -9,7 +9,7 @@ class GameController extends Controller
 
     protected function prepareGame(): string{
         ob_start();
-        include "WebGLBuild/index.html";
+        include "Views/game.php";
         return ob_get_clean();
     }
 
