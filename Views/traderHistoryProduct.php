c39fc7bf97dc4a527d555fe50d703552a7732b03
Retr0 <ngallot@myges.fr>
2021-06-27 19:55:00 +0200

    traderHistoryProduct

--- Views/traderHistoryProduct.php
+++ Views/traderHistoryProduct.php
@@ -38,10 +38,6 @@
                 <span class="input-group-text">Offre</span>
                 <input type="text" name="offer" class="form-control" style="text-align: center" readonly>
             </div>
-            <div class="input-group mb-1">
-                <span class="input-group-text">Contre Offre</span>
-                <input name="counter" type="text" class="form-control" style="text-align: center" readonly>
-            </div>
         </div>
     </div>
 </div>
