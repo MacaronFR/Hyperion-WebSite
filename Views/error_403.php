b323e7b49d9baf16bc54e3e313d1d36b65134d4f
CL4P-TR4P <mika-xuan@hotmail.com>
2021-06-27 12:53:14 +0200

    Update 403 - mika

--- Views/error_403.php
+++ Views/error_403.php
@@ -1,5 +1,5 @@
 <div class="container-fluid m-0 p-0 col-12" style="height: 100vh;">
-    <img src="imgClapi3.jpg" style="max-width: 100%;">
+    <img src="../assets/images/imgClapi3.jpg" style="max-width: 100%;">
     <div class="Centered" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
         <h1 style="color: lightgray">Error 403</h1>
         <h1 style="color: lightgray">Forbidden</h1>
