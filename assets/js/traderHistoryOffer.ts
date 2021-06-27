1ac3a71f4a3fd40bb667ededef692a28db54b4d0
Retr0 <ngallot@myges.fr>
2021-06-27 17:33:46 +0200

    traderHistoryOffer almost done

--- assets/js/traderHistoryOffer.ts
+++ assets/js/traderHistoryOffer.ts
@@ -23,23 +23,26 @@ function retrieve_history(params) {
             delete res['content']['totalNotFiltered'];
             for (let i = 0; i < Object.keys(res.content).length; ++i) {
                 rows.push(res.content[i]);
-                let color = (res.content[i]['status'] == 6) ? "primary" : res.content[i]['status'] == 5 ? "secondary" : "primary";
-                let buttonText = res.content[i]['status'] == 6 ? "Ok" : res.content[i]['status'] == 5 ? "deny" : "ok" ;
+                let color = (res.content[i]['status'] == 6) ? "primary" : "secondary";
+                let buttonText = (res.content[i]['status'] == 6) ? "ok" : "denied";
                 rows[i]['status'] = text['status'][rows[i]['status']];
-                console.log(text['status'][rows[i]['status']]);
                 rows[i]['state'] = text['state'][rows[i]['state']];
                 rows[i]['detail'] = "<button type=\"button\" class=\"btn btn-" + color + "\" data-offer-id=\"" + rows[i]['id'] + "\" data-offer-type=\"" + rows[i]['type'] + "\" data-offer-brand=\"" + rows[i]['brand'] + "\" data-offer-model=\"" + rows[i]['model'] + "\" data-offer-state=\"" + rows[i]['state'] + "\" data-offer=\"" + rows[i]['offer'] + "\" data-offer-counter=\"" + rows[i]['counter_offer'] + "\" onclick=\"seeDetail(this)\">" + text[buttonText] + "</button>"
             }
             params.success({"total": total, "totalNotFiltered": totalNotFiltered, "rows": rows});
         } else if (res.status.code === 204) {
-            $("#ToastWarning").children(".toast-body").text("Vous n'avez pas d'offre en attente");
+            $("#ToastWarning").children(".toast-body").text("Vous n'avez pas d'offre ");
             toastList[2].show();
             params.error()
         }
+
     })
 }
+
+
+
 function seeDetail(element) {
-    const detail = $("#divtraderpendinginfo");
+    const detail = $("#divtraderhistoryinfo");
     detail.find(".button-detail").remove();
     detail.find("[name=type]").val(element.dataset['offerType'])
     detail.find("[name=brand]").val(element.dataset['offerBrand'])
