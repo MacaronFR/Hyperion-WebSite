8b660810303163e3dffe6f24ac31597c579d58e3
Retr0 <ngallot@myges.fr>
2021-06-27 13:56:53 +0200

    traderHistoryOffer

--- assets/js/traderHistoryOffer.ts
+++ assets/js/traderHistoryOffer.ts
@@ -24,10 +24,10 @@ function retrieve_pending(params) {
             for (let i = 0; i < Object.keys(res.content).length; ++i) {
                 rows.push(res.content[i]);
                 let color = (res.content[i]['status'] == 5) ? "primary" : "secondary";
-                let buttonText = res.content[i]['status'] == 5 ? "accept" : "refuse";
+                let buttonText = res.content[i]['status'] == 5 ? "accept" : "detail";
                 rows[i]['status'] = text['status'][rows[i]['status']];
                 rows[i]['state'] = text['state'][rows[i]['state']];
-                rows[i]['refuse'] = "<button type=\"button\" class=\"btn btn-" + color + "\" data-offer-id=\"" + rows[i]['id'] + "\" data-offer-type=\"" + rows[i]['type'] + "\" data-offer-brand=\"" + rows[i]['brand'] + "\" data-offer-model=\"" + rows[i]['model'] + "\" data-offer-state=\"" + rows[i]['state'] + "\" data-offer=\"" + rows[i]['offer'] + "\" data-offer-counter=\"" + rows[i]['counter_offer'] + "\" onclick=\"seeDetail(this)\">" + text[buttonText] + "</button>"
+                rows[i]['detail'] = "<button type=\"button\" class=\"btn btn-" + color + "\" data-offer-id=\"" + rows[i]['id'] + "\" data-offer-type=\"" + rows[i]['type'] + "\" data-offer-brand=\"" + rows[i]['brand'] + "\" data-offer-model=\"" + rows[i]['model'] + "\" data-offer-state=\"" + rows[i]['state'] + "\" data-offer=\"" + rows[i]['offer'] + "\" data-offer-counter=\"" + rows[i]['counter_offer'] + "\" onclick=\"seeDetail(this)\">" + text[buttonText] + "</button>"
             }
             params.success({"total": total, "totalNotFiltered": totalNotFiltered, "rows": rows});
         } else if (res.status.code === 204) {
