836339a9e5fe8839f8247d0387a5e487bbb68aa7
Retr0 <ngallot@myges.fr>
2021-06-27 22:01:04 +0200

    traderHistoryProduct terminé

--- assets/js/traderHistoryOffer.js
+++ assets/js/traderHistoryOffer.js
@@ -19,7 +19,7 @@ function retrieve_history(params) {
             for (var i = 0; i < Object.keys(res.content).length; ++i) {
                 rows.push(res.content[i]);
                 var color = (res.content[i]['status'] == 6) ? "success" : "danger";
-                var button_1 = color == "success" ? "Validé" : "Refusé";
+                var button_1 = (color === "success") ? "Validé" : "Refusé";
                 rows[i]['status'] = text['status'][rows[i]['status']];
                 rows[i]['state'] = text['state'][rows[i]['state']];
                 rows[i]['detail'] = "<button type=\"button\" class=\"btn btn-" + color + "\" data-offer-id=\"" + rows[i]['id'] + "\" data-offer-type=\"" + rows[i]['type'] + "\" data-offer-brand=\"" + rows[i]['brand'] + "\" data-offer-model=\"" + rows[i]['model'] + "\" data-offer-state=\"" + rows[i]['state'] + "\" data-offer=\"" + rows[i]['offer'] + "\" onclick=\"seeDetail(this)\">" + text[button_1] + "</button>";
--- assets/js/traderHistoryOffer.ts
+++ assets/js/traderHistoryOffer.ts
@@ -24,7 +24,7 @@ function retrieve_history(params) {
             for (let i = 0; i < Object.keys(res.content).length; ++i) {
                 rows.push(res.content[i]);
                 let color = (res.content[i]['status'] == 6) ? "success" : "danger";
-                let button = color == "success" ? "Validé" : "Refusé";
+                let button = (color === "success") ? "Validé" : "Refusé";
                 rows[i]['status'] = text['status'][rows[i]['status']];
                 rows[i]['state'] = text['state'][rows[i]['state']];
                 rows[i]['detail'] = "<button type=\"button\" class=\"btn btn-" + color + "\" data-offer-id=\"" + rows[i]['id'] + "\" data-offer-type=\"" + rows[i]['type'] + "\" data-offer-brand=\"" + rows[i]['brand'] + "\" data-offer-model=\"" + rows[i]['model'] + "\" data-offer-state=\"" + rows[i]['state'] + "\" data-offer=\"" + rows[i]['offer'] + "\" onclick=\"seeDetail(this)\">" + text[button]  + "</button>"
