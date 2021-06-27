09dd2bc2efbb510caa072ccdba144df7cbe26dce
Retr0 <ngallot@myges.fr>
2021-06-27 13:48:37 +0200

    traderHistoryOffer

--- assets/js/traderHistoryOffer.ts
+++ assets/js/traderHistoryOffer.ts
@@ -23,15 +23,15 @@ function retrieve_pending(params) {
             delete res['content']['totalNotFiltered'];
             for (let i = 0; i < Object.keys(res.content).length; ++i) {
                 rows.push(res.content[i]);
-                let color = (res.content[i]['status'] == 4) ? "primary" : "secondary";
-                let buttonText = res.content[i]['status'] == 4 ? "counter" : "detail";
+                let color = (res.content[i]['status'] == 5) ? "primary" : "secondary";
+                let buttonText = res.content[i]['status'] == 5 ? "accept" : "refuse";
                 rows[i]['status'] = text['status'][rows[i]['status']];
                 rows[i]['state'] = text['state'][rows[i]['state']];
-                rows[i]['detail'] = "<button type=\"button\" class=\"btn btn-" + color + "\" data-offer-id=\"" + rows[i]['id'] + "\" data-offer-type=\"" + rows[i]['type'] + "\" data-offer-brand=\"" + rows[i]['brand'] + "\" data-offer-model=\"" + rows[i]['model'] + "\" data-offer-state=\"" + rows[i]['state'] + "\" data-offer=\"" + rows[i]['offer'] + "\" data-offer-counter=\"" + rows[i]['counter_offer'] + "\" onclick=\"seeDetail(this)\">" + text[buttonText] + "</button>"
+                rows[i]['refuse'] = "<button type=\"button\" class=\"btn btn-" + color + "\" data-offer-id=\"" + rows[i]['id'] + "\" data-offer-type=\"" + rows[i]['type'] + "\" data-offer-brand=\"" + rows[i]['brand'] + "\" data-offer-model=\"" + rows[i]['model'] + "\" data-offer-state=\"" + rows[i]['state'] + "\" data-offer=\"" + rows[i]['offer'] + "\" data-offer-counter=\"" + rows[i]['counter_offer'] + "\" onclick=\"seeDetail(this)\">" + text[buttonText] + "</button>"
             }
             params.success({"total": total, "totalNotFiltered": totalNotFiltered, "rows": rows});
         } else if (res.status.code === 204) {
-            $("#ToastWarning").children(".toast-body").text("Vous n'avez pas d'offre en attente");
+            $("#ToastWarning").children(".toast-body").text("Vous n'avez pas fait d'offre");
             toastList[2].show();
             params.error()
         }
@@ -44,47 +44,11 @@ const button = "<div class=\"d-flex justify-content-center mt-3 button-detail\">
     "</div>"
 
 function seeDetail(element) {
-    const detail = $("#divtraderpendinginfo");
+    const detail = $("#divtraderhistoryinfo");
     detail.find(".button-detail").remove();
     detail.find("[name=type]").val(element.dataset['offerType'])
     detail.find("[name=brand]").val(element.dataset['offerBrand'])
     detail.find("[name=model]").val(element.dataset['offerModel'])
     detail.find("[name=state]").val(element.dataset['offerState'])
     detail.find("[name=offer]").val(element.dataset['offer'] + " €")
-    if (element.dataset['offerCounter'] === "null") {
-        detail.find("[name=counter]").val("").attr("disabled", true)
-    } else {
-        detail.find("[name=counter]").val(element.dataset['offerCounter'] + " €").removeAttr("disabled");
-        detail.append(button);
-        detail.find(".accept").off("click").on("click", function () {
-            API_REQUEST("/offer/counter/accept/" + token + "/" + element.dataset['offerId'], "PUT").then((res) => {
-                if(res.status.code === 200) {
-                    $(".table").bootstrapTable("refresh");
-                    $("#ToastSuccess").children(".toast-body").text("Offre Acceptée");
-                    toastList[1].show();
-                }else{
-                    $("#ToastWarning").children(".toast-body").text("Erreur");
-                    toastList[1].show();
-                }
-            }).catch( () => {
-                $("#ToastError").children(".toast-body").text("Erreur");
-                toastList[0].show();
-            })
-        })
-        detail.find(".refuse").off("click").on("click", function () {
-            API_REQUEST("/offer/counter/refuse/" + token + "/" + element.dataset['offerId'], "PUT").then((res) => {
-                if (res.status.code === 200) {
-                    $(".table").bootstrapTable("refresh");
-                    $("#ToastWarning").children(".toast-body").text("Offre Refusée");
-                    toastList[2].show();
-                }else{
-                    $("#ToastWarning").children(".toast-body").text("Erreur");
-                    toastList[1].show();
-                }
-            }).catch( () => {
-                $("#ToastError").children(".toast-body").text("Erreur");
-                toastList[0].show();
-            })
-        })
-    }
 }
